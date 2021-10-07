690d93921c85e1b4c1c28bfc0e48f48f314a0122
someone <abhilashts94@gmail.com>
2021-10-07 11:28:02 +0530

    updated

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Info, Repos, User, Search, Navbar } from '../components'
+import loadingImage from '../images/preloader.gif'
+import { GithubContext } from '../context/context'
+const Dashboard = () => {
+  const { isLloading } = React.useContext(GithubContext)
+  if (isLloading) {
+    return (
+      <main>
+        <Navbar />
+        <Search />
+        <img src={loadingImage} alt='loading' className='loading-img' />
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Navbar></Navbar>
+      <Search />
+      <Info />
+      <User />
+      <Repos />
+    </main>
+  )
+}
+
+export default Dashboard
